8ab07cbc01d5a85bbe1cec9fd8ab78278d569741
Michal Bock <michal.bock@gmail.com>
2019-01-17 23:28:08 +0000

    Implement substrate consumer backend.

---
+++ proximo-server/substrate_consume.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+
+	"github.com/gofrs/uuid"
+	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/uw-labs/substrate"
+)
+
+// TODO: wrap errors with appropriate gRPC error codes
+
+type SourceInitialiser interface {
+	New(conf consumerConfig) (substrate.AsyncMessageSource, error)
+}
+
+type ackMessage struct {
+	id           string
+	substrateMsg substrate.Message
+}
+
+type substrateConsumeHandler struct {
+	Initialiser SourceInitialiser
+}
+
+func (h substrateConsumeHandler) HandleConsume(ctx context.Context, conf consumerConfig, forClient chan<- *Message, confirmRequest <-chan *Confirmation) error {
+	source, err := h.Initialiser.New(conf)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	eg, gCtx := errgroup.WithContext(ctx)
+
+	toAck := make(chan *ackMessage)
+	acks := make(chan substrate.Message)
+	messages := make(chan substrate.Message)
+
+	eg.Go(func() error {
+		return h.passMessagesToClient(gCtx, messages, forClient, toAck)
+	})
+	eg.Go(func() error {
+		return h.passAcksToSubstrate(gCtx, confirmRequest, acks, toAck)
+	})
+	eg.Go(func() error {
+		return source.ConsumeMessages(gCtx, messages, acks)
+	})
+
+	if err = eg.Wait(); err != nil {
+		return err
+	}
+
+	return ctx.Err()
+}
+
+func (h substrateConsumeHandler) passMessagesToClient(ctx context.Context, messages <-chan substrate.Message, forClient chan<- *Message, toAck chan<- *ackMessage) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case msg := <-messages:
+			ackM := &ackMessage{
+				id:           uuid.Must(uuid.NewV4()).String(),
+				substrateMsg: msg,
+			}
+			select {
+			case <-ctx.Done():
+				return nil
+			case toAck <- ackM:
+			}
+			pMsg := &Message{
+				Id:   ackM.id,
+				Data: msg.Data(),
+			}
+			select {
+			case <-ctx.Done():
+				return nil
+			case forClient <- pMsg:
+			}
+		}
+	}
+}
+
+func (h substrateConsumeHandler) passAcksToSubstrate(ctx context.Context, confirmRequest <-chan *Confirmation, acks chan<- substrate.Message, toAck <-chan *ackMessage) error {
+	ackMap := make(map[string]substrate.Message)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case aMsg := <-toAck:
+			ackMap[aMsg.id] = aMsg.substrateMsg
+		case conf := <-confirmRequest:
+			sMsg, ok := ackMap[conf.MsgID]
+			if !ok {
+				return errors.Errorf("invalid confirmation for message with id `%s`", conf.MsgID)
+			}
+			delete(ackMap, conf.MsgID)
+
+			select {
+			case <-ctx.Done():
+				return nil
+			case acks <- sMsg:
+			}
+		}
+	}
+}
