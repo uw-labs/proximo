8be1054208a7ee3d37ca356f6b03b158e4d82ca1
Simon Dickson <simonhdickson@users.noreply.github.com>
2020-08-13 14:58:42 +0100

    Add more tests

---
+++ backend/acl/source_test.go
@@ -0,0 +1,65 @@
+package acl
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/uw-labs/proximo/proto"
+)
+
+func Test_FactoryPublishOnlyClientCannotConsume(t *testing.T) {
+	assert := require.New(t)
+
+	c, err := ConfigFromString(fmt.Sprintf(`
+default:
+  roles: []
+roles:
+- id: "write-dogs"
+  publish: ["dogs.*"]
+clients:
+- id: "dog-publisher"
+  secret: "%s"
+  roles: ["write-dogs"]`, passwordHash))
+
+	s := AsyncSourceFactory{
+		Config: c,
+		Next:   backend{},
+	}
+
+	ctx := createCtx("dog-publisher", password)
+
+	_, err = s.NewAsyncSource(ctx, &proto.StartConsumeRequest{
+		Topic: "dogs",
+	})
+
+	assert.Equal(err, ErrUnauthorized)
+}
+
+func Test_FactoryConsumeOnlyClientCanConsume(t *testing.T) {
+	assert := require.New(t)
+
+	c, err := ConfigFromString(fmt.Sprintf(`
+default:
+  roles: []
+roles:
+- id: "read-dogs"
+  consume: ["dogs.*"]
+clients:
+- id: "dog-consumer"
+  secret: "%s"
+  roles: ["read-dogs"]`, passwordHash))
+
+	s := AsyncSourceFactory{
+		Config: c,
+		Next:   backend{},
+	}
+
+	ctx := createCtx("dog-consumer", password)
+
+	_, err = s.NewAsyncSource(ctx, &proto.StartConsumeRequest{
+		Topic: "dogs",
+	})
+
+	assert.NoError(err)
+}
